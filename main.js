4bd29537df66d623d9aaec2b82b2af28324d4a0a
LeonardoDuarteBonafe <leo_nardo136@hotmail.com>
2021-10-02 13:51:52 -0300

    Funções de clique adicionadas ao email e whatsapp

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
     $('.whatsapp').click(function(){
         console.log("whats");
+        window.open("https://api.whatsapp.com/send?phone=5528999854807&text=Hello, I am interested in your product!", '_blank');
     });
 
     $('.email').click(function(){
